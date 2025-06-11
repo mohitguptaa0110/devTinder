353b92e53d6514bdf4e46ebee465d5569c6b1a62
Mohit Gupta <mohitguptaa0110@gmail.com>
2025-06-12 00:01:59 +0530

    create an express server

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.use("/",(req,res) => {
+    res.send("Talking from the server!")
+})
+app.use("/hello",(req,res) => {
+    res.send("Hello from the server!")
+})
+app.use("/test",(req,res) => {
+    res.send("Test from the server!")
+})
+app.listen(3000, ()=>{
+    console.log("Server is successfull listening on port 3000....")
+})
